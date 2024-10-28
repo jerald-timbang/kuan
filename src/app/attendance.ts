11832a1e627e6b9358309e5e6586026e759253dd
jerald timbang <jeraldtimbang816@gmail.com>
2024-10-28 23:20:31 +0800

    mana

--- src/app/attendance.ts
+++
@@ -1,9 +0,0 @@
-export interface Attendance {
-  LRN_Number: string;
-  id: number;
-  created_at: string; // Column for timestamp
-  First_Name: string;      // First name
-  Last_Name: string;      // Last name
-  Middle_Initial: string;
-  status_marked: boolean; // Boolean for marked/unmarked status
-}
